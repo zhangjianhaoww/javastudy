7bf34c1b7f3667d828898758ec3a01ebe8dc44b6
zhangjianhaoww <zhangjianhaoww@outlook.com>
2020-06-01 22:34:47 +0800

    123

--- src/main/java/bilian/tech/zhang/javastarter/bst/BST.java
+++ src/main/java/bilian/tech/zhang/javastarter/bst/BST.java
@@ -2,6 +2,9 @@ package bilian.tech.zhang.javastarter.bst;
 
 import bilian.tech.zhang.javastarter.stack.ArrayStack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BST<E extends Comparable<E>> {
 
     private class Node{
@@ -187,24 +190,149 @@ public class BST<E extends Comparable<E>> {
         System.out.println(node.e);
     }
 
-    public static void main(String[] args) {
-        BST<Integer> bst = new BST<>();
-        int[] nums = {8, 5, 3, 9, 7};
-        for (int num : nums){
-            bst.add(num);
+    public void levelOrder(){
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(root);
+        while (!q.isEmpty()){
+            Node cur = q.remove();
+            System.out.println(cur.e);
+
+            if (cur.left != null){
+                q.add(cur.left);
+            }
+            if (cur.right != null){
+                q.add(cur.right);
+            }
+        }
+    }
+
+    /**
+     * 删除最小值和最大值
+     * 不一定是子节点
+     *
+     *
+     */
+
+    //寻找最小元素
+    public E minimum(){
+        if (size == 0){
+            throw new IllegalArgumentException("BST is empty!");
+        }
+        return minimum(root).e;
+    }
+
+    private Node minimum(Node node){
+        if (node.left == null){
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    //寻找最大元素
+    public E maximum(){
+        if (size == 0){
+            throw new IllegalArgumentException("BST is empty!");
+        }
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node){
+        if (node.right == null){
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+
+    /**
+     * 递归删除最小值
+     *
+     * @return
+     */
+    public E removeMin(){
+        E ret = minimum();
+
+        root = removeMin(root);
+        return ret;
+    }
+
+    private Node removeMin(Node node){
+        if (node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size --;    //删除操作
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+
+
+    public E removeMax(){
+        E ret = maximum();
+
+        root = removeMax(root);
+        return ret;
+    }
+
+    private Node removeMax(Node node){
+        if (node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size --;    //删除操作
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    public void remove(E e){
+
+    }
+
+    private Node remove(Node node, E e){
+
+        if (node == null){
+            return null;
+        }
+        if (e.compareTo(node.e) < 0){
+            node.left = remove(node.left, e);
+            return node;
+        }else if (e.compareTo(node.e) > 0){
+            node.right = remove(node.right, e);
+            return node;
+
+        }else{ //e == node.e{
+            if (node.left == null){ //left == null && right == null || left == null
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+            if (node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            //待删除节点的左右子树均不为空的情况
+            //找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            //用这个节点替代删除节点的位置
+            else{
+                Node successor = minimum(node.right);
+                successor.right = removeMin(node.right);  //size --
+                successor.left = node.left;
+                node.left = null;
+                node.right = null;
+
+                return successor;
+
+            }
         }
 
-        bst.preOrder();
-
-        System.out.println("\n" + bst);
-
-        bst.inOrder();
-
-        System.out.println();
-        bst.postOrder();
-
-        System.out.println();
-        bst.preOrder01();
     }
 
     /**
@@ -240,6 +368,42 @@ public class BST<E extends Comparable<E>> {
         }
         return sb.toString();
     }
+
+
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {8, 5, 3, 9, 7};
+        for (int num : nums){
+            bst.add(num);
+        }
+
+        bst.preOrder();
+
+        System.out.println("\n" + bst);
+
+        bst.inOrder();
+
+        System.out.println();
+        bst.postOrder();
+
+        System.out.println();
+        bst.preOrder01();
+
+        System.out.println();
+        bst.levelOrder();
+
+
+        System.out.println();
+        bst.removeMin();
+        bst.levelOrder();
+
+
+        System.out.println();
+        bst.removeMax();
+        bst.levelOrder();
+    }
+
 
 
 }
